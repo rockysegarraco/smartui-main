b90e94664d7ab9a8d612c30a77c3b31a9bca1900
Rocky Segarra <rockysegarra@icloud.com>
2024-01-08 17:31:00 -0500

    Update ButtonMap.js

--- src/components/Buttons/ButtonMap.js
+++ src/components/Buttons/ButtonMap.js
@@ -7,7 +7,7 @@ export default function ButtonMap() {
         <div className="rounded-full p-1.5 hover:bg-opacity-90 cursor-pointer text-center bg-[#006241] hover:bg-[#006241]/75">
           <div className="flex justify-center items-center">
             <img
-              className="h-8 pr-1 animate-bounce"
+              className="h-8 pr-1"
               src="img/map-map-marker-svgrepo-com.svg"
             />
 
