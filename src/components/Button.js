7c51dce4f24a46d5041b595c9f1ef6aaa4c721a3
Rocky Segarra <rockysegarra@icloud.com>
2024-01-12 11:09:46 -0500

    updated ui

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,25 +2,25 @@ import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 export default function Button(props) {
   return (
-    <div>
+    <div className="group">
       <div
         className={`flex w-full ${props.bg} ${props.animate} justify-between rounded-full p-1 items-center pr-4 hover:bg-opacity-90 cursor-pointer`}
       >
-        <div className="group flex grow flex-row items-center rounded-full justify-between">
+        <div className="flex grow flex-row items-center rounded-full justify-between">
+          <div className="flex items-center">
+            <div className="pr-4">
+              <img className="h-10" src={props.ImageUrl} alt="" />
+            </div>
+            <div
+              className={`uppercase font-medium text-md hover:text-sm ${props.textColor} text-center item-center group-hover:animate-jump-in`}
+            >
+              {props.title}
+            </div>
+          </div>
           <div>
-            <img
-              className="h-10 group-hover:animate-bounce"
-              src={props.ImageUrl}
-              alt=""
+            <ChevronRightIcon
+              className={`h-6 w-6 group-hover:animate-jump-in ${props.textColor}`}
             />
-          </div>
-          <div
-            className={`uppercase font-medium text-md ${props.textColor} text-center item-center`}
-          >
-            {props.title}
-          </div>
-          <div>
-            <ChevronRightIcon className={`h-6 w-6 ${props.textColor}`} />
           </div>
         </div>
       </div>
--- src/components/Buttons/ButtonMap.js
+++ src/components/Buttons/ButtonMap.js
@@ -5,7 +5,7 @@ export default function ButtonMap() {
   return (
     <div>
       <div className="pt-1">
-        <div className="text-blue-700 border border-blue-700 rounded-full py-2 hover:bg-opacity-90 cursor-pointer text-center bg-[#E9E9EE] hover:bg-blue-700 hover:text-white">
+        <div className="text-blue-700 border border-blue-700 rounded-full py-1 hover:bg-opacity-90 cursor-pointer text-center bg-[#E9E9EE] hover:bg-blue-700 hover:text-white">
           <div className="flex justify-center items-center">
             <MapPinIcon className="h-6 pr-1" />
 
