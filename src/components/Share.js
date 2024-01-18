be159a468b8300e9e32ba3d702319cf53abe34fc
Rocky Segarra <rockysegarra@icloud.com>
2024-01-18 09:45:44 -0500

    Update Share.js

--- src/components/Share.js
+++ src/components/Share.js
@@ -9,7 +9,7 @@ export default function SelectSmall() {
   return (
     <>
       <div className="flex items-center border-t bg-[#E9E9EE] sticky bottom-0">
-        <div className="basis-1/2 text-center text-[#E60023] border-r border-[#787878]/30 px-4 py-2">
+        <div className="basis-1/2 text-center text-[#E60023] border-r border-[#787878]/10 px-4 py-2">
           <img className="mx-auto h-12" src="img/Group 6.svg" />
         </div>
         <div className="basis-1/2 text-center text-[#E60023] px-4 py-2">
