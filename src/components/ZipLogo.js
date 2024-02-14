d817f55df1458f8dd1171f90cffee3166dbae57b
Rocky Segarra <rockysegarra@icloud.com>
2024-02-14 12:29:29 -0500

    Update ZipLogo.js

--- src/components/ZipLogo.js
+++ src/components/ZipLogo.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { MapPinIcon } from "@heroicons/react/24/solid";
 import { MapIcon } from "@heroicons/react/24/solid";
+import { XCircleIcon } from "@heroicons/react/24/solid";
 import Modal from "@/components/Modal";
 
 export default function Button(props) {
   const [open, setOpen] = useState(false);
   return (
     <div>
-      <div className="flex flex-row justify-between px-6 py-4 gap-y-4 mt-0 border-[#787878]/30">
-        <div className="text-[#4363F8] hover:text-blue-700 cursor-pointer item-center  hover:underline underline-offset-4">
-          <div className="flex flex-row items-center">
-            <div>
-              <MapPinIcon className="h-6 pr-1" />
+      <div className="flex flex-row items-center justify-between px-6 py-4 gap-y-4 mt-0 border-[#787878]/30">
+        <div className="basis-1/2 text-[#4363F8] hover:text-blue-700 cursor-pointer items-center  hover:underline underline-offset-4 p-2 border bg-white rounded-md">
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row items-center">
+              <div>
+                <MapPinIcon className="h-6 pr-1" />
+              </div>
+              <div className="underline underline-offset-4">32725</div>
             </div>
-            <div className="underline underline-offset-4">32725</div>
+            <div>
+              <XCircleIcon className="h-[20px] pl-8 text-slate-600" />
+            </div>
           </div>
         </div>
         <div>
