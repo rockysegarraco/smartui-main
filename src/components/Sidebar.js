aea2b1ddcde4b7450029a9d275beb8661c531fab
Rocky Segarra <rockysegarra@icloud.com>
2024-03-20 16:10:46 -0400

    update kroger poc

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,16 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { MinusCircleIcon } from "@heroicons/react/24/outline";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { TrashIcon } from "@heroicons/react/24/outline";
 
 import Button from "@/components/Button";
 
 export default function Example(props) {
   return (
     <Transition.Root show={props.isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={props.closeModal}>
+      <Dialog as="div" className="relative z-50" onClose={props.closeModal}>
         <div className="fixed inset-0" />
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
@@ -25,13 +28,15 @@ export default function Example(props) {
                   <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                     <div className="px-4 sm:px-6">
                       <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
-                          <img
-                            className="h-12"
-                            src="https://s3.us-west-2.amazonaws.com/retailerlogos.smartcommerce.co/img/supers/sc/us/small/targetpoweredbyinstacart_usa_dark.svg"
-                            alt=""
-                          />
-                        </Dialog.Title>
+                        <div className="flex items-center gap-x-4">
+                          <div className="font-bold text-xl">Your Basket</div>
+                          <div>
+                            <img
+                              className="h-8"
+                              src="https://click2cart.com/assets/img/basket.svg?20240319T050807194229"
+                            />
+                          </div>
+                        </div>
                         <div className="ml-3 flex h-7 items-center">
                           <button
                             type="button"
@@ -46,65 +51,76 @@ export default function Example(props) {
                       </div>
                     </div>
                     <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                      <div className="flex flex-row justify-between items-center gap-x-4 border p-4">
+                      <div className="flex flex-row items-center gap-x-4 border p-4">
                         <div>
                           <img
-                            className="h-12 w-full"
-                            src="https://s3-us-west-2.amazonaws.com/smartcommerce.co/images_pms/41/20708841.jpeg"
+                            className="w-[125px]"
+                            src="https://www.kroger.com/product/images/medium/front/0007143000034"
                             alt=""
                           />
                         </div>
                         <div className="grow text-sm">
-                          Pantene Miracle Moisture Boost Conditioner with Rose
-                          Water 8 fl oz
-                        </div>
-                        <div>$8.99</div>
-                        <div>
-                          <img src="erase.png" />
+                          <div className="font-bold text-black bg-[#FED241] text-center py-1 rounded-full w-[80px] mb-2">
+                            $3.67
+                          </div>
+                          Dole Sunflower Crunch Chopped Salad Kit 12.2 fl oz
+                          <div className="flex justify-between items-center">
+                            <div className="text-white mt-3 py-1 px-1 rounded-full text-center bg-[#E5E5E5] cursor-pointer lg:text-base text-xs w-[110px]">
+                              <div className="flex items-center justify-between gap-x-2">
+                                <div>
+                                  <MinusCircleIcon className="h-8 text-black" />{" "}
+                                </div>
+                                <div className="text-black text-base">1</div>
+                                <div>
+                                  <PlusCircleIcon className="h-8 text-black" />{" "}
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <TrashIcon className="h-6 text-slate-400" />
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <div className="flex flex-row justify-between items-center gap-x-4 border p-4">
+                      <div className="flex flex-row items-center gap-x-4 border p-4">
                         <div>
                           <img
-                            className="h-12 w-full"
-                            src="https://s3-us-west-2.amazonaws.com/smartcommerce.co/images_pms/41/20708841.jpeg"
+                            className="w-[125px]"
+                            src="https://www.kroger.com/product/images/medium/front/0007274506839"
                             alt=""
                           />
                         </div>
                         <div className="grow text-sm">
-                          Pantene Miracle Moisture Boost Conditioner with Rose
-                          Water 8 fl oz
-                        </div>
-                        <div>$8.99</div>
-                        <div>
-                          <img src="erase.png" alt="" />
-                        </div>
-                      </div>
-                      <div className="flex flex-row justify-between items-center gap-x-4 border p-4">
-                        <div>
-                          <img
-                            className="h-12 w-full"
-                            src="https://s3-us-west-2.amazonaws.com/smartcommerce.co/images_pms/41/20708841.jpeg"
-                            alt=""
-                          />
-                        </div>
-                        <div className="grow text-sm">
-                          Pantene Miracle Moisture Boost Conditioner with Rose
-                          Water 8 fl oz
-                        </div>
-                        <div>$8.99</div>
-                        <div>
-                          <img src="erase.png" alt="" />
+                          <div className="font-bold text-black bg-[#FED241] text-center py-1 rounded-full w-[80px] mb-2">
+                            $10.99
+                          </div>
+                          Dole Sunflower Crunch Chopped Salad Kit 12.2 fl oz
+                          <div className="flex justify-between items-center">
+                            <div className="text-white mt-3 py-1 px-1 rounded-full text-center bg-[#E5E5E5] cursor-pointer lg:text-base text-xs w-[110px]">
+                              <div className="flex items-center justify-between gap-x-2">
+                                <div>
+                                  <MinusCircleIcon className="h-8 text-black" />{" "}
+                                </div>
+                                <div className="text-black text-base">1</div>
+                                <div>
+                                  <PlusCircleIcon className="h-8 text-black" />{" "}
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <TrashIcon className="h-6 text-slate-400" />
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <div className="mt-4">
-                        <Button
-                          title="Add to Cart"
-                          ImageUrl="https://s3.us-west-2.amazonaws.com/retailerlogos.smartcommerce.co/img/supers/sc/us/small/targetpoweredbyinstacart_usa_dark.svg"
-                          bg="bg-[#CC0000]"
-                          animate="animate-delay-[.4s] animate-fade-up animate-once"
-                          textColor="text-white"
-                        />
+                      <div className="mt-0">
+                        <div className="flex items-center justify-between p-4 bg-slate-100 mb-2 mt-1 rounded-sm">
+                          <div className="font-bold">TOTAL</div>
+                          <div className="font-bold">$14.66</div>
+                        </div>
+                        <div className="flex justify-center text-white py-4 px-8 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs">
+                          Send to Cart
+                        </div>
                       </div>
                     </div>
                   </div>
---
+++ src/pages/kroger-poc.js
@@ -0,0 +1,151 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from "react";
+import PillsComplex from "@/components/Buttons/PillsComplex";
+import ButtonMap from "@/components/Buttons/ButtonMap";
+import Share from "@/components/Share";
+import LoadingScreen from "@/components/LoadingScreen";
+import Sidebar from "@/components/Sidebar";
+import { MapPinIcon } from "@heroicons/react/24/solid";
+
+function App() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // Simulate an API call
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 6000);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+  return (
+    <>
+      <Sidebar isOpen={isOpen} closeModal={closeModal} />
+      <div className="relative mx-auto max-w-7xl bg-slate-300">
+        <div className="bg-[#0E52A1] py-6 px-6 sticky top-0 z-10">
+          <div className="flex text-white items-center justify-between">
+            <div className="font-medium text-sm lg:text-2xl">
+              Choose your favorite products to save to your basket.
+            </div>
+            <div
+              className="flex justify-center text-white py-2 px-6 rounded-full text-center font-bold bg-white cursor-pointer lg:text-base text-xs"
+              onClick={openModal}
+            >
+              <div className="flex items-center justify-center gap-x-4">
+                <div className="text-black text-base">
+                  0 <span className="hidden lg:block">items</span>
+                </div>
+                <div>
+                  <img src="https://click2cart.com/assets/img/basket.svg?20240319T050807194229" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src="img/flyer_Page_01.jpg" />
+        </div>
+        <div class="absolute top-[20%] left-[58%] lg:top-[15%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[31%] left-[61%] lg:top-[26%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[34.5%] left-[23%] lg:top-[30.5%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[40.5%] left-[46%] lg:top-[37.5%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[41.75%] left-[82%] lg:top-[38.5%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[52%] left-[23%] lg:top-[50.5%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[52%] left-[53%] lg:top-[50.5%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[49.5%] left-[85%] lg:top-[47.5%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[63.5%] left-[28%] lg:top-[63.5%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[62%] left-[59%] lg:top-[62%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+        <div class="absolute top-[58%] left-[84%] lg:top-[56%]">
+          <div
+            className="flex justify-center text-white px-2 py-1 lg:py-2 lg:px-6 rounded-full text-center font-bold bg-[#1975CF]/95 cursor-pointer lg:text-base text-xs"
+            onClick={openModal}
+          >
+            + <span className="hidden lg:block">Add</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
