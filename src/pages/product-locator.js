07228fffeaccbde824011368553f3fd5a329eeda
Rocky Segarra <rockysegarra@icloud.com>
2025-02-05 14:00:06 -0500

    Create product-locator.js

---
+++ src/pages/product-locator.js
@@ -0,0 +1,355 @@
+export default function Page() {
+  const location = {
+    lat: 26.1188486,
+    lng: -80.13108199999999,
+  };
+
+  return (
+    <>
+      <style>{"body { background-color: #f5f5f7; }"}</style>
+      {/* WRAP */}
+      <div>
+        <div className="p-4">
+          <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-2 h-full">
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>
+                        299 E International Speedway Boulevard Deland, FL 32724
+                      </div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>ONLINE</div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>
+                        299 E International Speedway Boulevard Deland, FL 32724
+                      </div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>ONLINE</div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>
+                        299 E International Speedway Boulevard Deland, FL 32724
+                      </div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>ONLINE</div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>
+                        299 E International Speedway Boulevard Deland, FL 32724
+                      </div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+            {/* CARD */}
+            <div className="flex flex-col bg-white shadow h-full p-4 rounded">
+              {/* CONTENT CARD */}
+              <div className="grow">
+                <div className="flex gap-4 items-start">
+                  <div className="flex-none size-20">
+                    <img
+                      className="w-16"
+                      src="https://click2cart.com/img/supers/sc/us/small/walmartgrocery_usa_dark.svg"
+                    />
+                  </div>
+                  <div>
+                    {/* CONTENT FLEX */}
+                    <div className="flex flex-col gap-4 mb-4">
+                      <div>
+                        <span className="font-bold">Amazon</span> <br /> $26, 12
+                        oz (24 Pack)
+                      </div>
+                      <div>ONLINE</div>
+                      <div>
+                        <span className="font-bold">1.33mi</span>{" "}
+                        <span className="text-slate-100">|</span>{" "}
+                        <a className="text-[#4363F9]" href="#">
+                          Directions
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* FOOTER CARD */}
+              <div className="size-14 flex-none ...">
+                <button
+                  type="button"
+                  className="w-full rounded-full bg-[#4363F9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#5673FA] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+            {/* / CARD */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
